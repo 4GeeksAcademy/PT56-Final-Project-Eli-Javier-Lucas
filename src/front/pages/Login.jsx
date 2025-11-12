0bfa8825025d573a40bbd8bb31b062b79c54747f
Lucas Urquiza <105996657+LucasUr11@users.noreply.github.com>
2025-11-12 17:43:11 +0000

    Modificaciones en el formulario 'Login'

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -10,15 +10,17 @@ export const Login = () => {
                     <div className="text-center">
                         <h1 className="text-white pb-3">Iniciar Sesion</h1>
                     </div>
-                    <div className="mb-3">
+                    <div className="input-container mb-3">
+                        <i class="fa-solid fa-envelope"></i>
                         <input type="email" className="form-control" placeholder="Email" />
                     </div>
-                    <div className="mb-3">
+                    <div className="input-container mb-3">
+                        <i class="fa-solid fa-lock"></i>
                         <input type="password" className="form-control" placeholder="Contraseña" />
                     </div>
                     <div className="d-flex justify-content-center gap-2">
                         <button type="submit" className="btn btn-registro w-100">Iniciar sesion</button>
-                        <Link to="/login" className="btn btn-iniciar_sesion w-100">
+                        <Link to="/" className="btn btn-iniciar_sesion w-100">
                             Registrate
                         </Link>
                     </div>
